4bf6faeb489d244d7cea40248d72ce3ae98004e7
Nuno Edgar Nunes Fernandes <nunoedgar.engineering@gmail.com>
2026-03-02 19:36:48 +0000

    Update neuralengine_mlpregressor.py

--- PCF-neural-engine/neuralengine_mlpregressor.py
+++ PCF-neural-engine/neuralengine_mlpregressor.py
@@ -7,12 +7,12 @@ class PCFModel:
         # Load hyperparameters from our software-defined config
         self.config = load_yaml(config_path)
         self.engine = MLPRegressor([n_real, n_imag],
-            hidden_layer_sizes=self.config['ml_hyperparameters']['hidden_layers'],
+            hidden_layer_sizes=tuple(self.config['ml_hyperparameters']['hidden_layers'],
             activation='relu',
-            solver='adam'
-        )
+            solver='adam', random_state=42
+        ))
 
-    def predict_n_eff(self, wavelength, pitch, d_over_pitch):
+   def predict_n_eff(self, wavelength: float, pitch: float, d_over_pitch: float) -> np.ndarray:
         """
         Instantaneous prediction of the Complex Effective Index.
         Replaces 5 minutes of FEM simulation with 0.001s of inference.
